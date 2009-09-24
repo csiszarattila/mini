21ccdc2dddf6de3b93aace6b3b433f6711dfcd5b
Attila Csiszár <csiszar.ati@gmail.com>
2009-09-24 23:36:57 +0200

    Use Rack::Test and abandone Sinatra's built in test framework

--- test/base_spec.rb
+++ test/base_spec.rb
@@ -1,8 +1,42 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 describe 'Mini' do
+  include Rack::Test::Methods
+  
+  def app
+    Sinatra::Application
+  end
+    
   it 'should have default page' do
     get '/'
-    @response.should be_ok
+    
+    last_response.should be_ok
+  end
+  
+  it 'should have rss feed' do
+    get '/rss'
+    
+    last_response.should be_ok
+    last_response.headers['Content-Type'].should match('text/html')
+  end
+  
+  it 'should show documents in separate page by title' do
+    post = Post.all.first
+    post_prettified_title = post.prettify_filename
+    get '/bejegyzesek/' + post_prettified_title
+    
+    last_response.should be_ok
+    
+    article = Article.all.first
+    article_prettified_title = article.prettify_filename
+    get '/cikkek/' + article_prettified_title
+    
+    last_response.should be_ok
+    
+    no_doc_title = 'pretty-fied-title'
+    get '/cikkek/' + no_doc_title
+    
+    last_response.should_not be_ok
+    last_response.status.should be(404)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
-require 'sinatra'
-require 'sinatra/test/rspec'
 require File.dirname(__FILE__) + '/../mini'
+require 'spec'
+require 'rack/test'
+
