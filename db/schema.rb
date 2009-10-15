383b416817a3a9e79b09cc0e29e423baf4db3934
Attila Csiszár <csiszar.ati@gmail.com>
2009-10-15 23:06:32 +0200

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -2,10 +2,10 @@ require 'rubygems'
 require 'activerecord'
 ActiveRecord::Base.establish_connection(
 	:adapter => 'sqlite3',
-	:dbfile	=>	'db/mini.test.sqlite3.db'
+	:dbfile	=>	'db/mini.sqlite3.db'
 )
 
-ActiveRecord::Schema.define(:version=>2) do
+ActiveRecord::Schema.define(:version=>1) do
 	create_table :comments do |t|
 		t.string :name, :email, :website
 		t.text :body
