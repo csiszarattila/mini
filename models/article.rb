42f5826a0a91091a740bd959d83dc8233e0a2b39
Attila Csiszár <csiszar.ati@gmail.com>
2009-12-06 13:37:48 +0100

    Use different type of markups in articles

--- models/article.rb
+++ models/article.rb
@@ -2,4 +2,6 @@ require 'lib/commentable'
 
 class Article < ActiveDocument::Base
   include Commentable
+  
+  document_parsers( {".haml" => ActiveDocument::Parsers::Jaml, ".md" => ActiveDocument::Parsers::Yamd})
 end
